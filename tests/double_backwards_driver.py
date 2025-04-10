ec46a491663179bedf1b1bcb902368638b72646d
Austin Glover <austin_glover@berkeley.com>
2025-04-09 19:04:26 -0700

    driver always changes

--- tests/double_backwards_driver.py
+++ tests/double_backwards_driver.py
@@ -16,30 +16,20 @@ from openequivariance.benchmark.benchmark_configs import mace_nequip_problems
 
 
 implementations = [
-    LoopUnrollTP, 
-    E3NNTensorProduct,
+    # LoopUnrollTP, 
+    # E3NNTensorProduct,
     CUETensorProduct,
 ]
 
 problems = mace_nequip_problems
 
-# [
-#     # FullyConnectedTPProblem("1x1e", "1x1e", "1x1e"),
-#     ChannelwiseTPP("1x1e", "1x1e", "1x1e"),
-#     ChannelwiseTPP('32x0o + 32x0e + 32x1o + 32x1e', '0e + 1o', '32x0o + 32x0e + 32x1o + 32x1e')
-
-
-# ]
-
-
-
 directions : list[Direction] = [
-    # 'forward',
-    # 'backward',
+    'forward',
+    'backward',
     'double_backward',
 ]
 
-tests = [TestDefinition(implementation, problem, direction, correctness=False) for  problem, direction, implementation,  in product(problems, directions, implementations)]
+tests = [TestDefinition(implementation, problem, direction, correctness=True, benchmark=False) for  problem, direction, implementation,  in product(problems, directions, implementations)]
 
 if __name__ == "__main__":
 
