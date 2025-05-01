382251873a27914891d8e86a7ac207067d3129f1
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2025-04-30 22:16:52 -0700

    Deterministic double backward A kernel written.

--- openequivariance/templates/loop_unroll_conv_det.cuh
+++ openequivariance/templates/loop_unroll_conv_det.cuh
@@ -246,7 +246,95 @@ __global__ void double_backward_A(
         IRREP_T* L1_grad, IRREP_T* L2_grad, WEIGHT_T* W_grad, IRREP_T* L3_dgrad,
         ConvData c, void* workspace, unsigned {{idx_type}}* transpose_perm) {
 
-    printf("Hello world!");
+    extern __shared__ char s[];
+    size_t num_products = c.nnz;
+    unsigned {{idx_type}}* rows = (unsigned {{idx_type}}*) c.rows;
+    unsigned {{idx_type}}* cols = (unsigned {{idx_type}}*) c.cols;
+
+    IRREP_T* workspace = (IRREP_T*) workspace_raw;
+    {{idx_type}}* dst_idxs = ({{idx_type}}*) ((char*) workspace + {{forward_workspace_offset}}); 
+
+    if(lane_id == 0) {
+        if(start < end) {
+            dst_idxs[warp_id] = rows[start];
+        }
+        else {
+            dst_idxs[warp_id] = -1; 
+        }
+    }
+
+    {{ set_launch_bound_variables(forward_schedule.launch_config) }}
+    {%- set tpp = forward_schedule.updated_config %}
+    char* smem = s + {{forward_schedule.memory_per_warp}} * warp_loc; 
+
+    {%- for i, segment in enumerate(forward_schedule.segments) %} {
+        {{ declare_smem_variables(segment, "smem") }}
+        WEIGHT_T* w_buffer;
+
+        bool firstSegment = true;
+        ROW_OPERATION({{segment.L3.dim}}, j, L3_smem[j + lane_id] = 0.0f;)
+
+        for(size_t i = start; i < end; i++) {
+            unsigned {{idx_type}} row = rows[i]; unsigned {{idx_type}} col = cols[i];
+
+            IRREP_T* l1 = L1_in + col * {{forward_schedule.L1.dim}} + lane_id;
+            IRREP_T* l2 = L2_in + i * {{forward_schedule.L2.dim}} + lane_id; 
+            IRREP_T* l3 = L3_dgrad + row * {{forward_schedule.L3.dim}} + lane_id;
+
+            IRREP_T* l1_dgrad = L1_dgrad + col * {{forward_schedule.L1.dim}} + lane_id;
+            IRREP_T* l2_dgrad = L2_dgrad + i * {{forward_schedule.L2.dim}} + lane_id;
+
+            {%- if not tpp.shared_weights %} 
+                WEIGHT_T* w = W + i * {{tpp.weight_numel}};
+                WEIGHT_T* w_dgrad = W_dgrad + i * {{tpp.weight_numel}};
+            {%- else %}
+                WEIGHT_T* w = W;
+                WEIGHT_T* w_dgrad = W_dgrad;
+            {%- endif %}
+
+            __syncwarp();
+            {{ load_ir_segments(segment.L1Map, "l1", "L1_smem", "j") }}
+            {{ load_ir_segments(segment.L2Map, "l2", "L2_smem", "j") }}
+            ROW_OPERATION({{segment.L3.dim}}, j, L3_smem[j + lane_id] = 0.0f;)
+             
+            {%- if not forward_schedule.stream_weights %}
+                ROW_OPERATION({{segment.problem.weight_numel}}, j, weights_smem[j + lane_id] = w[{{segment.weight_offset}} + j + lane_id];)
+            {%- endif %}
+
+            w_buffer = w_dgrad;
+
+            for(int n = 0; n < 3; n++) {
+                if(n == 1) {
+                    {% if not forward_schedule.stream_weights%}
+                        ROW_OPERATION({{segment.problem.weight_numel}}, j, weights_smem[j + lane_id] = w[{{segment.weight_offset}} + j + lane_id];)
+                    {% endif %}
+                    {{ load_ir_segments(segment.L2Map, "l2_dgrad", "L2_smem", "j") }}
+                    w_buffer = w;
+                }
+                else if(n == 2) {
+                    {{ load_ir_segments(segment.L2Map, "l2", "L2_smem", "j") }}
+                    {{ load_ir_segments(segment.L1Map, "l1_dgrad", "L1_smem", "j") }}
+                }
+
+                __syncwarp();
+                forward_loop_unroll_{{i}}(L1_smem, L2_smem, w_buffer, weights_smem, L3_smem, scratch_smem, lane_id);
+                __syncwarp();
+            }
+
+            bool changeRow = (i < end - 1) && (row != rows[i+1]);
+            if(changeRow || i == end - 1) {
+                IRREP_T* dst = l3;
+                if(firstSegment) {
+                    dst = workspace + {{forward_schedule.L3.dim}} * warp_id + lane_id;
+                    firstSegment = false;
+                }
+                {{ store_ir_segments(segment.L3Map, "dst", "L3_smem", "j") }}
+                __syncwarp();
+
+                ROW_OPERATION({{segment.L3.dim}}, j, L3_smem[j + lane_id] = 0.0f;)
+            }
+        } 
+    } {%- endfor %}
 }
 
 
@@ -256,5 +344,5 @@ __global__ void double_backward_B(
         IRREP_T* L1_grad, IRREP_T* L2_grad, WEIGHT_T* W_grad, IRREP_T* L3_dgrad,
         ConvData c, void* workspace, unsigned {{idx_type}}* transpose_perm) {
 
-    printf("Hello world!");
+
 }
