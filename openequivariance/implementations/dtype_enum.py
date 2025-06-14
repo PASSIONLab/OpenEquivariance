46b37b753c4e2a01c50f36e1f594976571857bce
Austin Glover <austin_glover@berkeley.com>
2025-06-14 13:17:46 -0700

    better enum

--- openequivariance/implementations/dtype_enum.py
+++ openequivariance/implementations/dtype_enum.py
@@ -1,14 +1,29 @@
+from enum import IntEnum
+from types import MappingProxyType
 import numpy as np
 import torch
 
-PYTORCH_DTYPE_ENUM: dict[type[torch.dtype], int] = {
-    torch.float32: 1,
-    torch.float64: 2,
-    torch.int32: 3,
-}
 
-NUMPY_DTYPE_ENUM: dict[type[np.generic], int] = {
-    np.float32: 1,
-    np.float64: 2,
-    torch.int32: 3,
-}
+class DTypeEnum(IntEnum):
+    FLOAT32 = 1
+    FLOAT64 = 2
+    INT32 = 3
+    INT64 = 4
+
+
+dtype_to_enum_mapping = MappingProxyType(
+    {
+        torch.float32: DTypeEnum.FLOAT32,
+        torch.float64: DTypeEnum.FLOAT64,
+        torch.int32: DTypeEnum.INT32,
+        torch.int64: DTypeEnum.INT64,
+        np.float32: DTypeEnum.FLOAT32,
+        np.float64: DTypeEnum.FLOAT64,
+        np.int32: DTypeEnum.INT32,
+        np.int64: DTypeEnum.INT64,
+        np.dtype(np.float32): DTypeEnum.FLOAT32,
+        np.dtype(np.float64): DTypeEnum.FLOAT64,
+        np.dtype(np.int32): DTypeEnum.INT32,
+        np.dtype(np.int64): DTypeEnum.INT64,
+    }
+)
