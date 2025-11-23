d932d21a0cdae21fff71b0f6ebbcc86e4d7fe9d0
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2025-11-23 11:45:59 -0800

    Cleaned up code a bit.

--- openequivariance/extension/libjax_tp_jit.cpp
+++ openequivariance/extension/libjax_tp_jit.cpp
@@ -31,25 +31,25 @@ namespace ffi = xla::ffi;
 std::unordered_map<int64_t, std::unique_ptr<JITTPImpl<JITKernel>>> kernel_cache;
 std::mutex mut;
 
-std::unordered_map<string, int64_t> parse_launch_config(ffi::Dictionary dict) {
-    std::unordered_map<string, int64_t> result;
-    result["num_blocks"] = dict.get<int64_t>("num_blocks").value();
-    result["num_threads"] = dict.get<int64_t>("num_threads").value();
-    result["warp_size"] = dict.get<int64_t>("warp_size").value();
-    result["smem"] = dict.get<int64_t>("smem").value();
-    return result;
-}
+std::vector<std::string> launch_config_keys = {
+    "num_blocks", 
+    "num_threads", 
+    "smem"};
+std::vector<std::string> kernel_prop_keys = {
+    "L1_dim", 
+    "L2_dim", 
+    "L3_dim", 
+    "weight_numel", 
+    "shared_weights", 
+    "opt_level", 
+    "irrep_dtype", 
+    "weight_dtype"};
 
-std::unordered_map<string, int64_t> parse_kernel_prop(ffi::Dictionary dict) { 
+std::unordered_map<string, int64_t> parse_ffi_dict(ffi::Dictionary &dict, const std::vector<string> &keys) {
     std::unordered_map<string, int64_t> result;
-    result["L1_dim"] = dict.get<int64_t>("L1_dim").value();
-    result["L2_dim"] = dict.get<int64_t>("L2_dim").value();
-    result["L3_dim"] = dict.get<int64_t>("L3_dim").value();
-    result["weight_numel"] = dict.get<int64_t>("weight_numel").value();
-    result["shared_weights"] = dict.get<int64_t>("shared_weights").value();
-    result["opt_level"] = dict.get<int64_t>("opt_level").value();
-    result["irrep_dtype"] = dict.get<int64_t>("irrep_dtype").value();
-    result["weight_dtype"] = dict.get<int64_t>("weight_dtype").value();
+    for (const auto &key : keys) {
+        result[key] = dict.get<int64_t>(key).value();
+    }
     return result;
 }
 
@@ -67,13 +67,13 @@ JITTPImpl<JITKernel>* compile_kernel_with_caching(std::string_view kernel,
         if (it != kernel_cache.end()) {
             result = it->second.get(); 
         }
-        else {
+        else { 
             auto jit_tp_impl = std::make_unique<JITTPImpl<JITKernel>>(
                 std::string(kernel),
-                parse_launch_config(forward_config),
-                parse_launch_config(backward_config),
-                parse_launch_config(double_backward_config),
-                parse_kernel_prop(kernel_prop));
+                parse_ffi_dict(forward_config, launch_config_keys),
+                parse_ffi_dict(backward_config, launch_config_keys),
+                parse_ffi_dict(double_backward_config, launch_config_keys),
+                parse_ffi_dict(kernel_prop, kernel_prop_keys));
             result = jit_tp_impl.get();
             kernel_cache.insert({hash, std::move(jit_tp_impl)});
         }
@@ -86,7 +86,7 @@ ffi::Error tp_forward_impl(
         std::string_view kernel, ffi::Dictionary forward_config, ffi::Dictionary backward_config, ffi::Dictionary double_backward_config, ffi::Dictionary kernel_prop,
         int64_t hash, ffi::ResultBufferR0<ffi::S32> out) {
     
-    JITTPImpl<JITKernel>* jit_kernel = compile_kernel_with_caching(
+    auto jit_kernel = compile_kernel_with_caching(
         kernel, forward_config, backward_config, double_backward_config, kernel_prop, hash);
 
     std::cout << "SUCCESSFULLY COMPILED KERNEL!" << std::endl;
