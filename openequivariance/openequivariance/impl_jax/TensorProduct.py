d2cec505db96227fd79f7a610a92e7e4794daf2b
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2025-11-25 00:09:29 -0800

    Began putting together a test rig.

---
+++ openequivariance/openequivariance/impl_jax/TensorProduct.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+import jax
+import openequivariance_extjax as oeq_extjax 
+import hashlib
+from openequivariance.core.e3nn_lite import TPProblem
+
+for name, target in oeq_extjax.registrations().items():
+    jax.ffi.register_ffi_target(name, target, platform="CUDA")
+
+def hash_attributes(attrs):
+    m = hashlib.sha256()
+    for key in sorted(attrs.keys()):
+        m.update(attrs[key].__repr__().encode("utf-8"))
+
+    hash = int(m.hexdigest()[-16:], 16)
+    attrs["hash"] = hash
+
+class TensorProduct:
+    def __init__(self, problem: TPProblem):
+        self.problem = problem
+
+        self.kernel = "BLAH" 
+        self.forward_config = {"example_key": 42}
+        self.backward_config = {}
+        self.double_backward_config = {}
+        self.kernel_prop = {}
+        self.attrs = {
+            "kernel": self.kernel,
+            "forward_config": self.forward_config,
+            "backward_config": self.backward_config,
+            "double_backward_config": self.double_backward_config,
+            "kernel_prop": self.kernel_prop
+        }
+        hash_attributes(self.attrs)
+
+        self.forward_call = jax.ffi.ffi_call(
+            "tp_forward", 
+            jax.ShapeDtypeStruct((), jax.numpy.int32))
+        
+        self.forward_call(**self.attrs)
+
+
+if __name__ == "__main__":
+    tp_problem = None 
+    tensor_product = TensorProduct(tp_problem)
+    print("COMPLETE!")
